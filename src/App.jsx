50e1f650c8fb29007423b2e96a29b948d5e4573e
= <ululazmi4bs@gmail.com>
2023-09-17 09:14:25 +0700

    Adding content

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h2>Biodata</h2>
+      <ul>
+        <li>Ulul Azmi</li>
+        <li>Software Engineer</li>
+      </ul>
+    </div>
+  )
+}
+
+export {
+  App,
+};
